f9bc719011936e22ba555ed59a1f2789aac44030
afonse03 <amanda.m.fonseca03@gmail.com>
2021-07-16 16:05:09 -0600

    feat: iterating solutions (1-4)

--- js/iterating.js
+++ js/iterating.js
@@ -30,6 +30,7 @@
      * TODO:
      * Write some code that uses a for loop to log every item in the names array.
      */
+    console.log("For loop:");
     for (var i = 0; i < names.length; i++) {
         console.log("Name at index: " + i + " is " + names[i]);
     }
@@ -41,17 +42,17 @@
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
-    console.log("For loops");
+    console.log("ForEach loop:");
     names.forEach(function (element, index, array) {
-        console.log("Name at index: " + index + " is " + elements);
+        console.log("Name at index: " + index + " is " + element);
     });
 
-
+//
 
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
-     * return an an element from it
+     * return an element from it
      * - first: returns the first item in the array
      * - second: returns the second item in the array
      * - last: returns the last item in the array
@@ -61,5 +62,20 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+    function firstArray(array) {
+        return array[0];
+    }
+    console.log(firstArray(names));
 
+    function secondArray(array) {
+        return array[1];
+    }
+    console.log(secondArray(names));
+
+    function thirdArray(array) {
+        let i = array.length;
+        return (i - 1);
+    }
+    console.log(thirdArray(names));
 })();
+//
