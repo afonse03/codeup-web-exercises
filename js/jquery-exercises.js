c421662a71f1e63acc8b20cc5fe2519349e27887
afonse03 <amanda.m.fonseca03@gmail.com>
2021-08-18 14:43:41 -0600

    feat: working on jquery intro exercises

---
+++ js/jquery-exercises.js
@@ -0,0 +1,80 @@
+"use strict";
+$(document).ready(function() {
+
+
+    $(".menu-button").on("click", function() {
+        // alert("Hello");
+        $("li").toggleClass();
+
+
+    });
+    $(".login-button").on("click", function() {
+       // alert("hi");
+        $("h1").css("color", "pink");
+    });
+
+    $("#main-header")
+});
+
+/*
+Exercises
+Continue working in jquery_exercises.html for these exercises. Make sure you are committing your changes to GitHub.
+
+Id Selectors
+
+Create content in your HTML file using at least the following elements: h1, p, ul, li, div. DONE
+
+Add several attributes to your elements; you will need both id and class attributes. DONE
+
+Use jQuery to select an element by the id. Alert the contents of the element. DONE
+
+Update the jQuery code to select and alert a different id. DONE
+
+Use the same id on 2 elements. How does this change the jQuery selection? It will fire both, but one at a time, top to bottom. DONE
+
+Remove the duplicate id. Each id should be unique on that page. DONE
+*/
+
+/*
+Class Selectors
+
+Remove your custom jQuery code from previous exercises. DONE
+
+Update your code so that at least 3 different elements have the same class named codeup.
+
+Using jQuery, create a border around all elements with the class codeup that is 1 pixel wide and red.
+
+Remove the class from one of the elements. Refresh and test that the border has been removed.
+
+Give another element an id of codeup. Does this element get a border now?
+*/
+
+/* Element Selectors
+
+Remove your custom jQuery code from previous exercises.
+
+Using jQuery, set the font-size of all li elements to 20px.
+
+Craft selectors that highlight all the h1, p, and li elements.
+
+Create a jQuery statement to alert the contents of your h1 element(s).
+*/
+
+/*Multiple Selectors
+
+Combine your selectors that highlight all the h1, p, and li elements.
+
+ */
+
+//Mouse Events Exercise: 8/16/2021
+/*
+Use the file jquery_exercises.html for these exercises. Commit your changes to GitHub.
+
+Remove your custom jQuery code from previous exercises.
+
+Add jQuery code that will change the background color of an h1 element when clicked.
+
+Make all paragraphs have a font size of 18px when they are double clicked.
+
+Set all li text color to red when the mouse is hovering; reset to black when it is not.
+ */
